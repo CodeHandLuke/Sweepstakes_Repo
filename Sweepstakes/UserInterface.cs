a05d8caee4693fd69e94154cee67e06cd7074c7b
Luke Fleuri <lukefleuri@gmail.com>
2019-07-17 09:39:02 -0500

    Initial push

---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class UserInterface
+    {
+        //member variables
+
+
+
+
+
+        //constructor
+
+
+
+
+
+        //member methods
+    }
+}
