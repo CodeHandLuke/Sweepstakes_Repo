e808f84a2f5ee67eef4d123c62fadd714febb684
Luke Fleuri <lukefleuri@gmail.com>
2019-07-19 07:10:47 -0500

    Final submission

--- Sweepstakes/Program.cs
+++ Sweepstakes/Program.cs
@@ -10,29 +10,29 @@ namespace SweepstakesProject
     {
         static void Main(string[] args)
         {
-			//ISweepstakesManager managerProgram;
-			//string managerStyle = UserInterface.ChooseManagerProgram();
-			//managerProgram = ManagerFactory.ChooseManagerProgram(managerStyle);
-			//MarketingFirm bigShotMarketingFirm = new MarketingFirm(managerProgram);
+			ISweepstakesManager managerProgram;
+			string managerStyle = UserInterface.ChooseManagerProgram();
+			managerProgram = ManagerFactory.ChooseManagerProgram(managerStyle);
+			MarketingFirm bigShotMarketingFirm = new MarketingFirm(managerProgram);
 
 
-			//string sweepstakesGame = "freeCandy";
-   //         Sweepstakes sweepstakes = new Sweepstakes(sweepstakesGame);
-   //         Contestant c1 = new Contestant("Luke", "F", "Lukef@gmail.com");
-   //         Contestant c2 = new Contestant("Jon", "Doe", "jondoe@gmail.com");
-   //         Contestant c3 = new Contestant("Jane", "Doe", "jandoe@gmail.com");
-			//sweepstakes.RegisterContestant(c1);
-			//sweepstakes.RegisterContestant(c2);
-			//sweepstakes.RegisterContestant(c3);
+			string sweepstakesGame = "freeCandy";
+			Sweepstakes sweepstakes = new Sweepstakes(sweepstakesGame);
+			Contestant c1 = new Contestant("Luke", "F", "Lukef@gmail.com");
+			Contestant c2 = new Contestant("Jon", "Doe", "jondoe@gmail.com");
+			Contestant c3 = new Contestant("Jane", "Doe", "jandoe@gmail.com");
+			sweepstakes.RegisterContestant(c1);
+			sweepstakes.RegisterContestant(c2);
+			sweepstakes.RegisterContestant(c3);
 
-			//Console.WriteLine("\n");
-			//UserInterface.PrintContestantUI(c1);
-			//UserInterface.PrintContestantUI(c2);
-			//UserInterface.PrintContestantUI(c3);
-			//Console.WriteLine(sweepstakes.dictionaryContestant.Count);
-			//Console.ReadLine();
-			//sweepstakes.PickWinner();
-			//Console.ReadLine();
+			Console.WriteLine("\n");
+			UserInterface.PrintContestantUI(c1);
+			UserInterface.PrintContestantUI(c2);
+			UserInterface.PrintContestantUI(c3);
+			Console.WriteLine(sweepstakes.dictionaryContestant.Count);
+			Console.ReadLine();
+			sweepstakes.PickWinner();
+			Console.ReadLine();
 		}
     }
 }
